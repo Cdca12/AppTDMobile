f9b0e6c8e98798f01562a2210298387ce95143c7
Carlos Contreras <cdca12an@gmail.com>
2019-02-02 23:58:41 -0700

    Creación de proyecto
    Creación de proyecto en Android Studio con la API 19: Android 4.4 (KitKat). Añadido Activity principal con componentes básicos.

---
+++ app/src/main/java/com/example/cdca1/apptdmobile/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.cdca1.apptdmobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
