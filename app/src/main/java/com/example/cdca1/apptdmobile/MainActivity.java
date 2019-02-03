5c4fd3d0e75b238884457a409286ee8422d7fae5
Carlos Contreras <cdca12an@gmail.com>
2019-02-03 01:36:41 -0700

    Fullscreen
    Se añade el Fullscreen en el Home y se muestra el Toolbar en las demás pantallas.

--- app/src/main/java/com/example/cdca1/apptdmobile/MainActivity.java
+++ app/src/main/java/com/example/cdca1/apptdmobile/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.cdca1.apptdmobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // UI References
+    private TextView tv_registrarse;
+    private TextView tv_iniciar_sesion;
+    private TextView tv_ir_al_menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_registrarse = (TextView) findViewById(R.id.tv_registrarse);
+        tv_iniciar_sesion= (TextView) findViewById(R.id.tv_iniciar_sesion);
+        tv_ir_al_menu= (TextView) findViewById(R.id.tv_ir_al_menu);
+    }
+
+    public void goToLogin(View view) {
+        Intent i = new Intent(this, login.class);
+        startActivity(i);
     }
 }
