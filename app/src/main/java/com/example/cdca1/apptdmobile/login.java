4cdc38da1f019c85e72523b4bdfa2a2425371d53
Carlos Contreras <cdca12an@gmail.com>
2019-02-03 02:37:33 -0700

    Añadido Login
    Añadido Activity del Login y editado estilos, apegandose al diseño.

--- app/src/main/java/com/example/cdca1/apptdmobile/login.java
+++ app/src/main/java/com/example/cdca1/apptdmobile/login.java
@@ -2,12 +2,29 @@ package com.example.cdca1.apptdmobile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class login extends AppCompatActivity {
+
+    private EditText et_user;
+    private EditText et_password;
+    private Button bt_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        et_user = (EditText) findViewById(R.id.et_user);
+        et_password = (EditText) findViewById(R.id.et_password);
+    }
+
+    public void login(View view) {
+
+        bt_login = (Button) findViewById(R.id.bt_login);
+
+
     }
 }
